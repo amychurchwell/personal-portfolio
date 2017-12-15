ab6d9b3df2e9f407aba97bf624813e23e687d76e
Amy Churchwell <amy.churchwell@gmail.com>
2017-12-15 15:57:48 -0800

    Add card image transition on maximize

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,49 @@
 $('document').ready(function(){
+
+  // MAX AND MIN ON CARDS
   $('#plus-blue').click(function(){
     $('.card').removeClass('large');
-    $('.card:nth-child(1)').addClass('large blue');
+    $('.card:nth-child(2)').addClass('large blue');
+    $('.card:nth-child(2) .card-img').css('height', '50%');
   });
   $('#minus-blue').click(function(){
-    $('.card:nth-child(1)').removeClass('large blue');
+    $('.card:nth-child(2)').removeClass('large blue');
+    $('.card:nth-child(2) .card-img').css('height', '100%');
   });
 
   $('#plus-yellow').click(function(){
     $('.card').removeClass('large');
-    $('.card:nth-child(2)').addClass('large yellow');
+    $('.card:nth-child(3)').addClass('large yellow');
+    $('.card:nth-child(3) .card-img').css('height', '50%');
   });
   $('#minus-yellow').click(function(){
-    $('.card:nth-child(2)').removeClass('large yellow');
+    $('.card:nth-child(3)').removeClass('large yellow');
+    $('.card:nth-child(3) .card-img').css('height', '100%');
   });
 
   $('#plus-red').click(function(){
     $('.card').removeClass('large');
-    $('.card:nth-child(3)').addClass('large red');
+    $('.card:last-child').addClass('large red');
+    $('.card:last-child .card-img').css('height', '50%');
   });
   $('#minus-red').click(function(){
-    $('.card:nth-child(3)').removeClass('large red');
+    $('.card:last-child').removeClass('large red');
+    $('.card:last-child .card-img').css('height', '100%');
+  });
+
+  // COLOR CIRCLE PROPERTIES
+  $('.circles svg:first-child').click(function(){
+    $('.hero h1').removeClass('yellow red');
+    $('.hero h1').addClass('blue');
+  });
+
+  $('.circles svg:nth-child(2)').click(function(){
+    $('.hero h1').removeClass('red blue');
+    $('.hero h1').addClass('yellow');
+  });
+
+  $('.circles svg:last-child').click(function(){
+    $('.hero h1').removeClass('yellow blue');
+    $('.hero h1').addClass('red');
   });
 });
-
-// DRY UP THIS CODE
